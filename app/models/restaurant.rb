2fc21b70f2991950e153d45fe5c48a288959baf5
Joyce Chan <joycehwchan@gmail.com>
2022-11-09 14:19:36 +0900

    fix view

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,6 @@
 class Restaurant < ApplicationRecord
   CATEGORIES = %w[Chinese Italian Japanese French Belgian].freeze
-
   has_many :reviews, dependent: :destroy
-
   validates :name, presence: true
   validates :address, presence: true
   validates :category,
