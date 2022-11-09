2b132ea1bd0badc5bdf2a57855cde6944acc3b34
Joyce Chan <joycehwchan@gmail.com>
2022-11-09 14:14:39 +0900

    add restaurants and reviews

---
+++ app/models/restaurant.rb
@@ -0,0 +1,11 @@
+class Restaurant < ApplicationRecord
+  CATEGORIES = %w[Chinese Italian Japanese French Belgian].freeze
+
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category,
+            presence: true,
+            inclusion: { in: CATEGORIES }
+end
